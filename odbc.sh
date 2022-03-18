ffab49c3494da868edac63c3d85b7b3e254e8a10
Hassan Raza Pasha <38579192+paxha@users.noreply.github.com>
2022-03-19 02:10:48 +0500

    Update odbc.sh

--- odbc.sh
+++ odbc.sh
@@ -2,3 +2,10 @@
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install unixodbc python3-dev python3-pip python3-mysqldb -y
+cd /usr/src/asterisk-19.*/contrib/ast-db-manage/
+
+sudo nano config.ini.sample
+#sqlalchemy.url = mysql://asterisk:password@localhost/asterisk
+sudo cp config.ini.sample config.ini
+
+alembic -c config.ini upgrade head
