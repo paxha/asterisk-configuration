c999edc49a1a7e892a6366675610d2a8cd475263
Hassan Raza Pasha <38579192+paxha@users.noreply.github.com>
2022-03-19 01:54:59 +0500

    Update odbc.sh

--- odbc.sh
+++ odbc.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+sudo apt update && sudo apt upgrade -y
+sudo apt install mysql-server
+sudo systemctl start mysql.service
+sudo mysql_secure_installation
+sudo mysql
+CREATE DATABASE asterisk;
+CREATE USER 'asterisk'@'localhost' IDENTIFIED WITH MYSQL_NATIVE_PASSWORD BY 'password';
+GRANT ALL PRIVILEGES ON asterisk.* to 'asterisk'@'localhost';
+CREATE USER 'pasha'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT ALL PRIVILEGES ON *.* to 'pasha'@'%';
+FLUSH PRIVILEGES;
