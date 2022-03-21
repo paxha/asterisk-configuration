48d9bae9aba9537b0cc9f041c76a1a78bd39fb05
Hassan Raza Pasha <38579192+paxha@users.noreply.github.com>
2022-03-21 16:59:30 +0500

    Update complete_installation.sh

--- complete_installation.sh
+++ complete_installation.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install wget build-essential subversion -y
+cd /usr/src
+sudo wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-19-current.tar.gz
+sudo tar zxf asterisk-19-current.tar.gz
+cd asterisk-19.*/
+sudo contrib/scripts/get_mp3_source.sh
+sudo contrib/scripts/install_prereq install
+sudo ./configure
+sudo make menuselect
+sudo make
+sudo make install
+sudo make samples
+sudo make config
+sudo ldconfig
+sudo groupadd asterisk
+sudo useradd -r -d /var/lib/asterisk -g asterisk asterisk
+sudo usermod -aG audio,dialout asterisk
+sudo chown -R asterisk.asterisk /etc/asterisk
+sudo chown -R asterisk.asterisk /var/{lib,log,spool}/asterisk
+sudo chown -R asterisk.asterisk /usr/lib/asterisk
+sudo echo 'AST_USER="asterisk"' >> /etc/default/asterisk
+sudo echo 'AST_GROUP="asterisk"' >> /etc/default/asterisk
+sudo echo 'runuser = asterisk;' >> /etc/asterisk/asterisk.conf
+sudo echo 'rungroup = asterisk;' >> /etc/asterisk/asterisk.conf
+sudo systemctl restart asterisk
+
 
